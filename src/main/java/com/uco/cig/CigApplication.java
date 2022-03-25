d9f521251a86dd99f089fe71eb2327b79b9d6e44
Antonio Alzate <antonioalzate7@gmail.com>
2022-03-25 17:14:46 -0500

    Commit Inicial.

---
+++ src/main/java/com/uco/cig/CigApplication.java
@@ -0,0 +1,13 @@
+package com.uco.cig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uco/cig/CigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uco.cig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
